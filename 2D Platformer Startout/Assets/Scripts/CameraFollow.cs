dc1e96fdc36bf4e1435ac71b08540b3e1cff3b35
preyansh <preyansh@ualberta.ca>
2017-01-29 15:37:13 -0700

    Started working on a camera follow script
    working on camera follow script,, hopefully i can get it working

--- 2D Platformer Startout/Assets/Scripts/CameraFollow.cs
+++ 2D Platformer Startout/Assets/Scripts/CameraFollow.cs
@@ -1,44 +1,40 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class CameraFollow : MonoBehaviour{
 
-    private Vector2 velocity;
+public class CameraFollow : MonoBehaviour {
 
-    //floats
-    public float smoothTimeY;
-    public float smoothTimeX;
+	private Vector2 velocity;
 
-    public GameObject player;
+	//floats
+	public float smoothTimeY;
+	public float smoothTimeX;
 
-    //if you wanna make boundaries
-    public bool bounds;
+	public GameObject player;
 
-    public Vector3 minCameraPos;
-    public Vector3 maxCameraPos;
+	//if you wanna make boundaries
+	public bool bounds;
 
-    // Use this for initialization
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
+	public Vector3 minCameraPos;
+	public Vector3 maxCameraPos;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player 0");
+	}
 
-    //does the work for making sure the camera follows the player
+	// Update is called once per frame
+	void FixedUpdate() {
+		float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y,ref velocity.y, smoothTimeY);
+		//again z needs to be taken account of as it is 2D, just make it not do anything
+		transform.position = new Vector3(posX, posY, transform.position.z);
+		if(bounds)
+		{
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+				Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+				Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+		}
+	}
 
-    void FixedUpdate()
-    {
-        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x,ref velocity.x,smoothTimeX);
-        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y,ref velocity.y, smoothTimeY);
-
-        //again z needs to be taken account of as it is 2D, just make it not do anything
-        transform.position = new Vector3(posX, posY, transform.position.z);
-
-        if(bounds)
-        {
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
-                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
-                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
-        }
-    }
-    
 }
